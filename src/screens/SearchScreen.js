36658d8eebfed4d176a31cfa330237deaa386558
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-04-30 22:22:23 +0530

    Showing Search Results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,6 +4,7 @@ import React,{useState,useEffect} from 'react';
 import {View,Text,StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
@@ -20,6 +21,10 @@ const SearchScreen = () => {
                 ? <Text>{errorMessage}</Text> 
                 : null } 
             <Text>We have found {results.length} results.</Text>
+
+            <ResultsList title="Cost Effective" />
+            <ResultsList title="Bit Pricier" />
+            <ResultsList title="Big Spender" />
         </View>
     );
 };
