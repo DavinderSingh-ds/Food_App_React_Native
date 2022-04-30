b45b02fc19f08e43a137158e875436864821661a
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-04-30 21:23:59 +0530

    Api linked

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,24 +1,41 @@
 import React,{useState} from 'react';
 import {View,Text,StyleSheet} from 'react-native';
-
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
 
-    //fetch is used fot making network requests
+    //this state is created for yelp api objects
+    const [results, setResults] = useState([]);
 
+    //helper function
+    // '/search' is a route linked with api
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search',{
+            params: {
+                limit : 50,
+                term,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses);
+        
+    }
+
+    //fetch is used fot making network requests
     //axios separate library to making requests
 
     return(
         <View>
             <SearchBar 
                 term={term}
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={searchApi}
                 />
             <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found {results.length} results.</Text>
         </View>
     );
 };
