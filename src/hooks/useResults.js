6287dcc7ab611783c9cb216b67e1d0089559a576
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-04-30 23:20:26 +0530

    Changing Location and check

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -18,7 +18,7 @@ export default () => {
                 params: {
                     limit : 50,
                     term: searchTerm,
-                    location: 'san jose'
+                    location: 'nashville'
                 }
             });
             setResults(response.data.businesses);
